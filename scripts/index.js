cefea71ad88e1a612195fd3dacd8c8b5673c8e3e
anas20023 <anasibnebelal400@gmail.com>
2026-02-22 16:35:12 +0600

    Event Delegation on the Parent of job items

--- scripts/index.js
+++ scripts/index.js
@@ -10,12 +10,28 @@ document.addEventListener('DOMContentLoaded', () => {
     // console.log(totalCount)
 })
 const showAllJobs = () => {
+    if(jobCards.length<=0){
+        const child=document.createElement("section")
+        child.className='max-w-6xl mx-auto'
+        child.innerHTML=
+
+            `
+                <div class="flex flex-col justify-center items-center">
+                <img class="w-20 h-20" src="../jobs.png" >
+                <p class="font-bold text-[#002C5C]">No jobs available</p>
+                <p class="text-sm text-slate-500">Check back soon for new job opportunities</p>
+                </div>
+            `
+
+        jobContainer.appendChild(child)
+        return
+    }
 
     for (const job of jobCards) {
         const child = document.createElement('div')
         child.innerHTML = `
     
-                    <div class="flex flex-col justify-between gap-2 bg-white outline-2 outline-gray-100 p-4 rounded border-l-4 border-slate-200">
+                    <div class="flex flex-col justify-between gap-2 bg-white outline-2 outline-gray-100 p-4 rounded border-l-4 ${(job.status==="reject")?"border-red-400":(job.status==="interview")?"border-green-400":"border-slate-400"}">
                     <div class="flex flex-row justify-between items-center">
                         <div>
                             <h1 class="text-lg font-bold text-[#002C5C]">${job.companyName}</h1>
@@ -27,7 +43,7 @@ const showAllJobs = () => {
                         </div>
                     </div>
                     <p class="text-sm text-gray-500">${job.location + "  •  "+ job.type +"  •  "+ job.salary}</p>
-                    <span class="bg-[#EEF4FF] w-32 text-center rounded p-2 mt-1">NOT APPLIED</span>
+                    <span class="${(job.status==="reject")?"bg-red-400 text-white":(job.status==="interview")?"bg-green-400 text-white":"bg-slate-400"} w-32 text-center rounded p-2 mt-1">${(job.status==="reject")?"REJECT":(job.status==="interview")?"APPLIED":"NOT APPLED"}</span>
                     <p class="text-sm text-gray-500">${job.description}</p>
                     <div class="flex flex-row gap-2 pt-2">
                         <button  class="btn btn btn-outline btn-success interview_btn">Interview</button>
