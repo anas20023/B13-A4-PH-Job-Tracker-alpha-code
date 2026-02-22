b89a2cb090a578c7076925e38287b957b42c9daf
anas20023 <anasibnebelal400@gmail.com>
2026-02-22 14:37:58 +0600

    Some Event propagations :(

--- scripts/switch.js
+++ scripts/switch.js
@@ -1,4 +1,9 @@
 const interviewButton=document.querySelectorAll(".interview_btn")
 const rejectButton=document.querySelectorAll(".reject_btn")
-console.log(interviewButton)
-console.log(rejectButton)
+// console.log(interviewButton)
+// console.log(rejectButton)
+interviewButton.forEach((btn)=>{
+    btn.addEventListener("click",(e)=>{
+        console.log(e.target.parentNode.parentNode)
+    })
+})
